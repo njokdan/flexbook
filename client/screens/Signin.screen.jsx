dbccbe09c871a886ac8ab91050bcab86bb00eb47
ariel <ariel@ariel.com>
2020-07-04 01:47:48 +0300

    fixed some margins on front, ready for production - devBundle commented (express.js)

--- client/screens/Signin.screen.jsx
+++ client/screens/Signin.screen.jsx
@@ -4,6 +4,7 @@ import { Redirect } from "react-router-dom";
 import { signin } from "../api/auth.api";
 
 import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
@@ -19,8 +20,6 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     marginTop: theme.spacing(5),
     paddingBottom: theme.spacing(2),
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
   },
   error: {
     verticalAlign: "middle",
@@ -89,50 +88,56 @@ export default function Signin(props) {
   }
 
   return (
-    <Card className={classes.card} elevation={2}>
-      <CardContent>
-        <Typography variant="h6" className={classes.title}>
-          Sign In
-        </Typography>
-        <TextField
-          id="email"
-          type="email"
-          label="Email"
-          className={classes.textField}
-          value={values.email}
-          onChange={handleChange("email")}
-          margin="normal"
-        />
-        <br />
-        <TextField
-          id="password"
-          type="password"
-          label="Password"
-          className={classes.textField}
-          value={values.password}
-          onChange={handleChange("password")}
-          margin="normal"
-        />
-        <br />
-        {values.error && (
-          <Typography component="p" color="error">
-            <Icon color="error" className={classes.error}>
-              error
-            </Icon>
-            {values.error}
-          </Typography>
-        )}
-      </CardContent>
-      <CardActions>
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={clickSubmit}
-          className={classes.submit}
-        >
-          Submit
-        </Button>
-      </CardActions>
-    </Card>
+    <Grid container justify="center">
+      <Grid item sm={false} md={2} />
+      <Grid item sm={12} md={8}>
+        <Card className={classes.card} elevation={2}>
+          <CardContent>
+            <Typography variant="h6" className={classes.title}>
+              Sign In
+            </Typography>
+            <TextField
+              id="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              value={values.email}
+              onChange={handleChange("email")}
+              margin="normal"
+            />
+            <br />
+            <TextField
+              id="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              value={values.password}
+              onChange={handleChange("password")}
+              margin="normal"
+            />
+            <br />
+            {values.error && (
+              <Typography component="p" color="error">
+                <Icon color="error" className={classes.error}>
+                  error
+                </Icon>
+                {values.error}
+              </Typography>
+            )}
+          </CardContent>
+          <CardActions>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={clickSubmit}
+              className={classes.submit}
+            >
+              Submit
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+      <Grid item sm={false} md={2} />
+    </Grid>
   );
 }
